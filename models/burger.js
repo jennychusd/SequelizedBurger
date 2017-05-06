7cc678d4605a226d54f10db63db7db9a56c01029
Jenny Chu <jennychu.sd@gmail.com>
2017-05-05 19:13:47 -0700

    added models

--- models/burger.js
+++ models/burger.js
@@ -1,23 +1,18 @@
-var orm = require("../config/orm.js");
-
-var burger = {
-  selectAll: function(cb) {
-    orm.selectAll("burgers", function(res) {
-      cb(res);
-    });
-  },
-  // The variables cols and vals are arrays.
-  insertOne: function(cols, vals, cb) {
-    orm.insertOne("burgers", cols, vals, function(res) {
-      cb(res);
-    });
-  },
-  updateOne: function(objColVals, condition, cb) {
-    orm.updateOne("burgers", objColVals, condition, function(res) {
-      cb(res);
-    });
-  }
-};
-
-// Export the database functions for the controller (catsController.js).
-module.exports = burger;
+module.exports = function (sequelize, DataTypes) {
+  var Burger = sequelize.define("Burger", {
+    id: {
+      type: DataTypes.INTEGER,
+      autoIncrement: true,
+      primaryKey: true,
+    },
+    burger: {
+      type: DataTypes.STRING,
+      allowNull: false
+    },
+    devoured: {
+      type: DataTypes.BOOLEAN,
+      default: false
+    }
+  });
+  return Burger;
+}
